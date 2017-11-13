c89705b66da83d206c202c4572b6bc49d5fd9366
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-13 02:56:19 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -1,1 +1,22 @@
 'use strict';
+
+class Question {
+  static All() {
+    return this._All
+  }
+
+  static Find(id) {
+    return this.All()[id-1]
+  }
+
+  constructor(content) {
+    this.content = content;
+    this.save()
+  }
+
+  save() {
+    this.constructor._All.push(this)
+  }
+}
+
+Question._All = []
